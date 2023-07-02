e17a9ca75b1bb691a0abaf3a18772a5da3071aea
stormnrage <santifriesen.cod@gmail.com>
2023-07-02 18:55:09 -0300

    prest3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-let nombre = prompt ("ingresar nombre")
-            let prestamo = prompt("ingresar monto");
-            let cuotas = prompt("cantidad de cuotas")            
-            if ((cuotas == 18) || (cuotas < 18) && (cuotas != 0)) { alert ("elegiste un credito a " + cuotas + " cuotas")}
-            else { alert("la cantidad de coutas no esta permitida")}
 
-            let prestamo2 = "Hola! " + nombre + " el monto a devolver " + prestamo * 1.1 **cuotas ;
-            alert (prestamo2)
+let nombre = prompt("Ingresar nombre");
+let prestamo = prompt("Ingresar monto");
+let cuotas = prompt("Cantidad de cuotas");
+
+while (cuotas < 1 || cuotas > 18) {
+  alert("La cantidad de cuotas no está permitida. Por favor, ingresa un valor entre 1 y 18.");
+  cuotas = prompt("Cantidad de cuotas");
+}
+
+alert("Elegiste un crédito a " + cuotas + " cuotas");
+
+let total = (prestamo * Math.pow(1.1, cuotas)).toFixed(2);
+let cuota = (total / cuotas).toFixed(2);
+
+let prestamo2 =
+  "Hola, " +
+  nombre +
+  ". El monto a devolver es " +
+  total +
+  " y el monto de cada cuota es " +
+  cuota;
+
+alert(prestamo2);
+            
